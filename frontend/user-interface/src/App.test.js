c27837914c9aff3681d3c0bbd33ea9e72fc0ae74
Saurabh <kulkarni.saur@husky.neu.edu>
2019-06-07 03:35:43 -0400

    Adding the initial test for the CreatePostForm Component

--- frontend/user-interface/src/App.test.js
+++ frontend/user-interface/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
+import CreatePostForm from './components/CreatePostForm';
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<CreatePostForm />);
+    expect(wrapper.find('Form')).to.have.length(1);
+  });
 });
