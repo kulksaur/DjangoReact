8347ec507db160c5e7bf7a51a8dfaeb38aaf81c6
Saurabh <kulkarni.saur@husky.neu.edu>
2019-06-07 03:46:39 -0400

    Adding the second test for the PostsTable component

--- frontend/user-interface/src/App.test.js
+++ frontend/user-interface/src/App.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
 import CreatePostForm from './components/CreatePostForm';
+import PostsTable from './components/PostsTable';
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() });
-describe('App component testing', function() {
-  it('renders welcome message', function() {
+describe('Check if the CreatePostForm Component has Form in it', function() {
+  it('Checks presence', function() {
     const wrapper = shallow(<CreatePostForm />);
     expect(wrapper.find('Form')).to.have.length(1);
   });
 });
+
+describe('Check if the PostsTable Component has table in it', function() {
+  it('Checks presence', function(){
+    const wrapper = shallow(<PostsTable />);
+    expect(wrapper.find('Table')).to.have.length(1);
+  })
+})
+
